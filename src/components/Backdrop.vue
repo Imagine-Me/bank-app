c614ae024d160c8d5b6a893025ac32a49be2a62b
Imagine-Me <prince100thomas@gmail.com>
2021-04-27 17:01:53 +0530

    Resolve esline issues

--- src/components/Backdrop.vue
+++ src/components/Backdrop.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="flex justify-center items-center fixed inset-0 bg-gray-400 bg-opacity-50 z-30">
-        <slot></slot>
-    </div>
+  <div
+    class="flex justify-center items-center fixed inset-0 bg-gray-400 bg-opacity-50 z-30"
+  >
+    <slot></slot>
+  </div>
 </template>
