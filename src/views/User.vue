075ed4d279ee34c70edf2cac0f992c24d09cb7bf
Imagine-Me <prince100thomas@gmail.com>
2021-04-29 12:08:31 +0530

    Add user page

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,13 +1,25 @@
 <template>
   <HomeHeader>User</HomeHeader>
+  <div class="m-auto max-w-xs bg-white p-2 mt-5">
+    <label for="">Name</label>
+    <input class="border w-full p-1" type="text" readonly :value="name" />
+    <label class="mt-2" for="">Account Number</label>
+    <input class="border w-full p-1" type="text" readonly :value="accountNo" />
+    <label for="">Balance</label>
+    <input class="border w-full p-1" type="text" readonly :value="balance" />
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 import HomeHeader from "../components/HomeHeader";
 
 export default {
   components: {
     HomeHeader,
+  },
+  computed: {
+    ...mapState(["name", "accountNo", "balance"]),
   },
 };
 </script>
